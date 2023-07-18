efb8122d681d50677d9de2e0fa3df92ffa89e506
yVitorXD <vitorxdxx@gmail.com>
2023-07-18 17:22:45 -0300

    Singleton

--- src/PadroesCriacionais/singleton/Cliente.java
+++ src/PadroesCriacionais/singleton/Cliente.java
@@ -12,6 +12,23 @@ public class Cliente {
 			conn.query("SELECT * FROM A1");
 	}
 	
+	public static void doQuery2() {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A2");
+	}
 	
-	
+	public static void doQuery3() {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A3");
+	}
+
+	public static void main(String[] args) {
+		doQuery1();
+		doQuery2();
+		doQuery3();
+	}
 }
